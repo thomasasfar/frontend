21e8babdf08b45aaae1aec50ef6446edccb27335
Thomas Nobel Asfar <2111521019_thomas@student.unand.ac.id>
2023-06-25 22:22:04 +0700

    profile nii

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,64 +1,103 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col, Card, Button, Form, Image } from "react-bootstrap";
-import "../styles/Profile.css"
-import profilePhoto from "../assets/images/profil.svg"
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+  Form,
+  Image,
+} from "react-bootstrap";
+import "../styles/Profile.css";
+import profilePhoto from "../assets/images/profil.svg";
 
 const Profile = () => {
-  
   return (
-    <section className="vh-100" style={{ backgroundColor: '#f4f5f7' }}>
+    <section className="vh-100" style={{ backgroundColor: "#f4f5f7" }}>
       <Container className="py-5 h-100">
         <Row className="d-flex justify-content-center align-items-center">
-          <Col md={6} lg={6}>
-            <Card style={{ borderRadius: '.5rem' }}>
+          <Col md={8} lg={8}>
+            <Card style={{ borderRadius: ".5rem" }}>
               <Card.Body className="row g-0">
                 <Col md={4} className="text-center">
                   <Image
                     src={profilePhoto}
                     alt="Avatar"
                     className="img-fluid my-5"
-                    style={{ width: '120px' }}
+                    style={{ width: "120px" }}
                     rounded
                   />
-                  <Button className="button-ganti-foto">Change Picture</Button>
+                  <Button className="button-ganti-foto" variant="dark">
+                    Change Picture
+                  </Button>
                 </Col>
                 <Col md={8}>
                   <div className="card-body p-4">
-                    <h6>Information</h6>
+                    <h6>My Profile</h6>
                     <hr className="mt-0 mb-4" />
-                    <div className="row pt-1">
-                      <div className="col-6 mb-3">
-                        <h6>Email</h6>
-                        <p className="text-muted">info@example.com</p>
-                      </div>
-                      <div className="col-6 mb-3">
-                        <h6>Phone</h6>
-                        <p className="text-muted">123 456 789</p>
-                      </div>
-                    </div>
-                    <h6>Projects</h6>
-                    <hr className="mt-0 mb-4" />
-                    <div className="row pt-1">
-                      <div className="col-6 mb-3">
-                        <h6>Recent</h6>
-                        <p className="text-muted">Lorem ipsum</p>
-                      </div>
-                      <div className="col-6 mb-3">
-                        <h6>Most Viewed</h6>
-                        <p className="text-muted">Dolor sit amet</p>
-                      </div>
-                    </div>
-                    <div className="d-flex justify-content-start">
-                      <a href="#!">
-                        <i className="fab fa-facebook-f fa-lg me-3"></i>
-                      </a>
-                      <a href="#!">
-                        <i className="fab fa-twitter fa-lg me-3"></i>
-                      </a>
-                      <a href="#!">
-                        <i className="fab fa-instagram fa-lg"></i>
-                      </a>
-                    </div>
+
+                    <Row>
+                      <Form id="profile">
+                        <Form.Group
+                          as={Row}
+                          className="form-outline mb-3"
+                          controlId="nama"
+                        >
+                          <Form.Label column sm={3}>
+                            Nama
+                          </Form.Label>
+                          <Col sm={9}>
+                            <Form.Control
+                              type="text"
+                              placeholder="Enter your name"
+                              disabled
+                            />
+                          </Col>
+                        </Form.Group>
+
+                        <Form.Group
+                          as={Row}
+                          className="form-outline mb-3"
+                          controlId="username"
+                        >
+                          <Form.Label column sm={3}>
+                            Username
+                          </Form.Label>
+                          <Col sm={9}>
+                            <Form.Control
+                              type="text"
+                              placeholder="Enter your name"
+                              disabled
+                            />
+                          </Col>
+                        </Form.Group>
+
+                        <Form.Group
+                          as={Row}
+                          className="form-outline mb-3 "
+                          controlId="email"
+                        >
+                          <Form.Label column sm={3}>
+                            Email
+                          </Form.Label>
+                          <Col sm={9}>
+                            <Form.Control
+                              type="email"
+                              placeholder="Enter your name"
+                              disabled
+                            />
+                          </Col>
+                        </Form.Group>
+                      </Form>
+                    </Row>
+                    <Row>
+                      <Col md={{ span: 2, offset: 8 }} sm={6}>
+                        <Button variant="secondary" style={{ visibility: "hidden" }}>Cancel</Button>
+                      </Col>
+                      <Col md={{ span: 2,}} sm={6}>
+                        <Button variant="primary">Edit</Button>
+                      </Col>
+                    </Row>
                   </div>
                 </Col>
               </Card.Body>
