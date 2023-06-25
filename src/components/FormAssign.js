9d017047b1429f3f9709aa0b53db8864f0c63666
Thomas Nobel Asfar <2111521019_thomas@student.unand.ac.id>
2023-06-25 13:58:37 +0700

    .

--- src/components/FormAssign.js
+++ src/components/FormAssign.js
@@ -9,6 +9,8 @@ import {
   Modal,
 } from "react-bootstrap";
 import axios from "axios";
+import ReactPaginate from "react-paginate";
+import "bulma/css/bulma.css";
 
 const FormAsign = () => {
   const [show, setShow] = useState(false);
@@ -22,16 +24,50 @@ const FormAsign = () => {
   const [formHeader, setFormHeader] = useState("");
   const [formDescription, setFormDescription] = useState("");
 
+  // pagination
+  const [page, setPage] = useState(0);
+  const [limit, setLimit] = useState(5);
+  const [pages, setPages] = useState(0);
+  const [rows, setRows] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const [query, setQuery] = useState("");
+  const [msg, setMsg] = useState("");
+
+
+
   useEffect(() => {
-    axios
-      .get("http://localhost:3000/forms/formMe", { withCredentials: true })
-      .then((response) => {
-        setForms(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+    getForms();
+  }, [page, keyword]);
+
+  const getForms = async () => {
+    const response = await axios.get(
+      `http://localhost:3000/forms?page=${page}&limit=${limit}`
+      // `http://localhost:3000/forms`
+    );
+    setForms(response.data.result);
+    setPage(response.data.page);
+    setPages(response.data.totalPage);
+    setRows(response.data.totalRows);
+    console.log(response.data.result);
+  };
+
+  const changePage = ({ selected }) => {
+    setPage(selected);
+    if (selected === 9) {
+      setMsg(
+        "Jika tidak menemukan data yang Anda cari, silahkan cari data dengan kata kunci spesifik!"
+      );
+    } else {
+      setMsg("");
+    }
+  };
+
+  const searchData = (e) => {
+    e.preventDefault();
+    setPage(0);
+    setMsg("");
+    setKeyword(query);
+  };
 
   const handleModalOpen = (form) => {
     setSelectedForm(form);
@@ -143,7 +179,7 @@ const FormAsign = () => {
           </Col>
         </Row>
         <Table striped hover>
-          <thead>
+          <thead >
             <tr>
               <th>No</th>
               <th>Date</th>
@@ -177,6 +213,29 @@ const FormAsign = () => {
             ))}
           </tbody>
         </Table>
+        <p>
+          Total Rows: {rows} Page: {rows ? page + 1 : 0} of {pages}
+        </p>
+        <p className="has-text-centered has-text-danger">{msg}</p>
+        <nav
+          className="pagination is-centered"
+          key={rows}
+          role="navigation"
+          aria-label="pagination"
+        >
+          <ReactPaginate
+            previousLabel={"< Prev"}
+            nextLabel={"Next >"}
+            pageCount={Math.min(10, pages)}
+            onPageChange={changePage}
+            containerClassName={"pagination-list"}
+            pageLinkClassName={"pagination-link"}
+            previousLinkClassName={"pagination-previous"}
+            nextLinkClassName={"pagination-next"}
+            activeLinkClassName={"pagination-link is-current"}
+            disabledLinkClassName={"pagination-link is-disabled"}
+          />
+        </nav>
       </Container>
 
       <Modal show={show} onHide={handleClose}>
