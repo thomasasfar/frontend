2d6b51c7d7709309e33f264f442410ec1abfc66c
Thomas Nobel Asfar <2111521019_thomas@student.unand.ac.id>
2023-06-25 10:27:21 +0700

    .

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -8,39 +8,21 @@ const Dash = () => {
   return (
     <div className="badan-dash">
       <Container className="mt-5 mb-5 px-5 pb-5">
-        <Row>
-          <i>Welcome </i>
-        </Row>
-          <Row>
-            <Col sm={6}>
+      <Row>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col sm={6}>
             <Container className="kolom border">
-              <Row>
-                <Col sm={6} className="border">
-                  <h1>HE</h1>
-                </Col>
-              </Row>
-              <Row>
-                <Col sm={6} className="border">
-                  <h1>HE</h1>
-                </Col>
-              </Row>
+                <Row>
+                    <Col sm={6} className="border">
+                        <h1>HE</h1>
+                    </Col>
+                </Row>
             </Container>
-          </Col>
-          <Col sm={6} className="border">
-            sm=4
-          </Col>
-        </Row>
-        <Row>
-          <Col>sm=true</Col>
-        </Row>
-          </Col>
-          <Col sm={6} className="border">
-            sm=4
-          </Col>
-        </Row>
-        <Row>
-          <Col>sm=true</Col>
-        </Row>
+        </Col>
+        <Col sm={6} className="border">sm=4</Col>
+      </Row>
       </Container>
     </div>
   );
